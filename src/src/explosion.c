7afb941300a069fa221ff58faa58db1c9bb978f2
jzeiber <23410344+jzeiber@users.noreply.github.com>
2021-09-05 16:14:26 -0400

    Better explosions

--- src/src/explosion.c
+++ src/src/explosion.c
@@ -42,10 +42,16 @@ void Explosion_Create(struct Game *g, struct GameObject *o)
 			// explosion origin is line segment midpoint of object
 			e->m_obj.m_pos.m_x=(o->m_worldcoords[i].m_x+o->m_worldcoords[ni].m_x)/2.0;
 			e->m_obj.m_pos.m_y=(o->m_worldcoords[i].m_y+o->m_worldcoords[ni].m_y)/2.0;
+			// add a little bit of outward movement from the center of the explosion
+			const double outrad=_atan2(-(e->m_obj.m_pos.m_y-o->m_pos.m_y),(e->m_obj.m_pos.m_x-o->m_pos.m_x));
+			const double dx=(_cos(outrad)*.1)+(_cos(e->m_obj.m_movrad)*e->m_obj.m_movspeed);
+			const double dy=-((_sin(outrad)*.1)+(_sin(e->m_obj.m_movrad)*e->m_obj.m_movspeed));
+			e->m_obj.m_movrad=_atan2(-dy,dx);
+			e->m_obj.m_movspeed=_sqrt((dx*dx)+(dy*dy));
 			const double len=_sqrt((o->m_worldcoords[i].m_x-o->m_worldcoords[ni].m_x)*(o->m_worldcoords[i].m_x-o->m_worldcoords[ni].m_x)+(o->m_worldcoords[i].m_y-o->m_worldcoords[ni].m_y)*(o->m_worldcoords[i].m_y-o->m_worldcoords[ni].m_y));
 			double rad=_atan2(-(o->m_worldcoords[i].m_y-o->m_worldcoords[ni].m_y),(o->m_worldcoords[i].m_x-o->m_worldcoords[ni].m_x));
 			e->m_obj.m_rotrad=rad;
-			e->m_obj.m_rotspeed=_drand()*0.1;
+			e->m_obj.m_rotspeed=(_drand()*0.1)-0.5;
 			e->m_obj.m_coordcount=2;
 			e->m_obj.m_modelcoords[0].m_x=-(len/2.0);
 			e->m_obj.m_modelcoords[0].m_y=0;
